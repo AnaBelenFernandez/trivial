326c8c3fd7cbd94e31b1fb3618a06b20b01472f5
Sergio Revilla <sergyma18@gmail.com>
2022-01-21 17:01:22 +0100

    constructor jPanel_pregunta. Metodos para tratar con jPanel_Pregunta

--- Hoja_6_Ejercicio_1/src/main/java/Interface/JPanel_pregunta.java
+++ Hoja_6_Ejercicio_1/src/main/java/Interface/JPanel_pregunta.java
@@ -5,7 +5,16 @@
  */
 package Interface;
 
+import BaseDatos.Conexion;
 import BaseDatos.GestorDB;
+import Modelo.Respuesta;
+import java.awt.Frame;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 import javax.swing.ButtonGroup;
 
 /**
@@ -33,6 +42,15 @@ public class JPanel_pregunta extends javax.swing.JPanel
      
 
     }
+    
+     public JPanel_pregunta(Frame parent, boolean modal) {
+        
+        initComponents();
+         setSize(500,500);
+        
+       
+    }
+     
     public void cargarPregunta(){
     }
 public void colorear(){
@@ -40,6 +58,107 @@ public void colorear(){
 
 
 }
+
+public void preguntarAUsuario() throws SQLException
+    {
+        Scanner teclado = new Scanner(System.in);
+        String sqlPreguntas = "SELECT id, enunciado, veces_formulada, veces_acertada "
+                + "FROM preguntas ORDER BY RAND() LIMIT 4";
+
+        PreparedStatement consultaPreguntas = Conexion.getInstance().getConnection().prepareStatement(sqlPreguntas);
+        ResultSet resultPreguntas = consultaPreguntas.executeQuery();
+        System.out.println("¡Que comience el juego!");
+        while (resultPreguntas.next())
+        {
+            int id = resultPreguntas.getInt("id");
+            String enunciado = resultPreguntas.getString("enunciado");
+            int veces_formulada = resultPreguntas.getInt("veces_formulada");
+            int veces_acertada = resultPreguntas.getInt("veces_acertada");
+
+            System.out.printf("%-5s %-10s\n", id, enunciado);
+            //obtenemos las respuestas de dicha pregunta y las mostramos
+            List<Respuesta> respuestas = respuestasPorPregunta(id);
+            int contador = 1;
+            String cadenaRespuestas = "";
+            for (Respuesta respuesta : respuestas)
+            {
+                cadenaRespuestas += contador + " " + respuesta.getTexto() + "\n";
+                contador++;
+            }
+            System.out.println(cadenaRespuestas);
+
+            //el usuario responde 1/2/3/4 en funcion de la respuesta. Se comprueba si dicha respuesta es correcta
+            System.out.println("Introduzca su respuesta: (1/2/3/4)");
+            int respuestaUsuario = teclado.nextInt();
+            teclado.nextLine();
+            if (respuestas.get(respuestaUsuario - 1).isCorrecta())
+            {
+                System.out.println("Enhorabuena, ¡respuesta acertada!");
+                aumentarPreguntaAcertada(veces_acertada, id);
+            } else
+            {
+                System.out.println("¡Oh vaya! ha fallado. Intentelo de nuevo D: ");
+            }
+            //Contabilizamos las respuestas en la base de datos
+            aumentarRespuestaRespondida(respuestas.get(respuestaUsuario - 1).getVeces_respondida(), id);
+            aumentarPreguntaFormulada(veces_formulada, id);
+        }
+    }
+
+//Devuelve una lista de respuestas posibles a una pregunta
+    public List<Respuesta> respuestasPorPregunta(int idPregunta) throws SQLException
+    {
+        List<Respuesta> respuestas = new ArrayList();
+        String sqlRespuestas = "SELECT * FROM respuestas WHERE pregunta_id=?";
+        PreparedStatement consultaRespuestas = Conexion.getInstance().getConnection().prepareStatement(sqlRespuestas);
+        consultaRespuestas.setInt(1, idPregunta);
+        ResultSet result = consultaRespuestas.executeQuery();
+
+        while (result.next())
+        {
+            Respuesta r = new Respuesta();
+
+            r.setId(result.getInt("id"));
+            //r.setPregunta(result.getObject("pregunta_id"));
+            r.setTexto(result.getString("texto"));
+            r.setCorrecta(result.getBoolean("correcta"));
+            r.setFoto(result.getString("foto"));
+            r.setVeces_respondida(result.getInt("veces_respondida"));
+            respuestas.add(r);
+        }
+        return respuestas;
+    }
+
+    //Aumenta el numero de veces que una pregunta es formulada
+    public void aumentarPreguntaFormulada(int veces_formulada, int id) throws SQLException
+    {
+        String sql = "UPDATE preguntas SET veces_formulada=? WHERE id=?";
+        PreparedStatement update = Conexion.getInstance().getConnection().prepareStatement(sql);
+        update.setInt(1, veces_formulada + 1);
+        update.setInt(2, id);
+        update.executeUpdate();
+        // System.out.println("Pregunta formulada " + veces_formulada + 1 + " veces");
+    }
+
+    //Aumenta el numero de veces que una pregunta es acertada
+    public void aumentarPreguntaAcertada(int veces_acertada, int id) throws SQLException
+    {
+        String sql = "UPDATE preguntas SET veces_acertada=? WHERE id=?";
+        PreparedStatement update = Conexion.getInstance().getConnection().prepareStatement(sql);
+        update.setInt(1, veces_acertada + 1);
+        update.setInt(2, id);
+        update.executeUpdate();
+    }
+
+    //Aumenta el numero de veces que se responde una respuesta
+    public void aumentarRespuestaRespondida(int veces_respondida, int id) throws SQLException
+    {
+        String sql = "UPDATE respuestas SET veces_respondida=? WHERE id=?";
+        PreparedStatement update = Conexion.getInstance().getConnection().prepareStatement(sql);
+        update.setInt(1, veces_respondida + 1);
+        update.setInt(2, id);
+        update.executeUpdate();
+    }
 
     /**
      * This method is called from within the constructor to initialize the form. WARNING: Do NOT
